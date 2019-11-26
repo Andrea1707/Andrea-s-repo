f5d2cf6a4a441aedb1723140fd47ac5fb5366338
Andrea <andrea.ferrario170700@gmail.com>
2019-11-25 21:34:22 -0800

    /

--- Computer Science/Java Projects/Lab6/Lab6Esercizio1/Lab6Esercizio1.java
+++ Computer Science/Java Projects/Lab6/Lab6Esercizio1/Lab6Esercizio1.java
@@ -6,7 +6,7 @@ public class Lab6Esercizio1 {
     public static void main(String[] args) {
 
     int [][] matrix = Matrix.RandomMatrix(2, 5, 10);
-    System.out.print( Matrix.ToString (matrix));
+    System.out.print( Matrix.ToString (matrix)); 
 
     System.out.print(Matrix.MatrixInfo(matrix));
         
